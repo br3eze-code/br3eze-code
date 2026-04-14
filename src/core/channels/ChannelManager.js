ba1e112d6286eff1963a1e06b05e7367ab10eeca
Brighton Mzacana <markertwithbrighton@gmail.com>
2026-04-14 20:06:56 +0200

    Update ChannelManager.js

--- src/core/channels/ChannelManager.js
+++ src/core/channels/ChannelManager.js
@@ -52,6 +52,19 @@ class ChannelManager extends EventEmitter {
       await channel.send(msg.userId, this.formatResponse(result));
     });
 
+    // Forward special events (QR, status updates, commands, etc.)
+    channel.on('qr', (qr) => {
+      this.emit('qr', { channel: type, qr });
+    });
+
+    channel.on('command', (cmd) => {
+      this.emit('command', { channel: type, ...cmd });
+    });
+
+    channel.on('status', (status) => {
+      this.emit('status', { channel: type, status });
+    });
+
     await channel.initialize();
     this.channels.set(type, channel);
     
@@ -110,50 +123,5 @@ class ChannelManager extends EventEmitter {
   }
 }
 
-// src/core/channels/BaseChannel.js
-class BaseChannel extends EventEmitter {
-  constructor(config, agent) {
-    super();
-    this.config = config;
-    this.agent = agent;
-    this.id = crypto.randomUUID();
-    this.connected = false;
-    this.messageCount = 0;
-    this.errorCount = 0;
-  }
+module.exports = ChannelManager;
 
-  async initialize() {
-    throw new Error('Not implemented');
-  }
-
-  async send(userId, message) {
-    throw new Error('Not implemented');
-  }
-
-  async broadcast(message) {
-    throw new Error('Not implemented');
-  }
-
-  formatMessage(message) {
-    // Default formatting - override in subclasses
-    if (typeof message === 'string') {
-      return { text: message };
-    }
-    return message;
-  }
-
-  getStatus() {
-    return {
-      id: this.id,
-      connected: this.connected,
-      messages: this.messageCount,
-      errors: this.errorCount
-    };
-  }
-
-  async destroy() {
-    this.removeAllListeners();
-  }
-}
-
-module.exports = { ChannelManager, BaseChannel };
