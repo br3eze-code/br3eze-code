bd83b7035cd45ba7cc83dca08b814b7fba7b7cc1
Brighton Mzacana <markertwithbrighton@gmail.com>
2026-09-12 08:48:51 +0200

    feat: export framework compatibility adapters

---
+++ src/adapters/frameworks/index.js
@@ -0,0 +1,9 @@
+export { AgentOSFrameworkAdapter, assertAdapterContract } from './AgentOSFrameworkAdapter.js';
+export {
+  createOpenAIAgentsAdapter,
+  createMicrosoftAgentFrameworkAdapter,
+  createLangGraphAdapter,
+  createCrewAIAdapter,
+  createAutoGenAdapter,
+  loadOptionalAdapter
+} from './optional.js';
