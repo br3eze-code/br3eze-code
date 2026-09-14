f21ede4baec54fe91b77ee4ee63edb7a027b401b
Brighton Mzacana <markertwithbrighton@gmail.com>
2026-09-14 20:28:30 +0200

    fix: align channel barrel with actual adapter exports

--- src/channels/index.js
+++ src/channels/index.js
@@ -1,11 +1,11 @@
 import { BaseChannel } from './base.js';
-import { WhatsAppChannel } from './whatsapp.js';
-import { TelegramChannel } from './telegram.js';
-import DiscordChannel from './discord.js';
-import SlackChannel from './slack.js';
+import WhatsAppChannel from './whatsapp.js';
+import TelegramChannel from './telegram.js';
+import { DiscordChannel } from './discord.js';
+import { SlackChannel } from './slack.js';
 import SMSAdapter from './sms.adapter.js';
 import WebAdapter from './web.adapter.js';
-import WebSocketChannel from './websocket.js';
+import { WebSocketChannel } from './websocket.js';
 
 /**
  * Canonical channel adapter barrel.
