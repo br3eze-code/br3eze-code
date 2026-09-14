c5a7f6fdcedd7650f655dbc45d15fb548c47ece7
Brighton Mzacana <markertwithbrighton@gmail.com>
2026-09-14 16:24:21 +0200

    feat: publish canonical AgentOS plugin SDK surface

---
+++ src/sdk/plugin/index.js
@@ -0,0 +1,6 @@
+export { Plugin, isPlugin } from './Plugin.js';
+export { PluginManifest, normalizeManifest } from './PluginManifest.js';
+export { PluginContext } from './PluginContext.js';
+export { PluginRegistry } from './PluginRegistry.js';
+export { PluginLoader } from './PluginLoader.js';
+export const PLUGIN_API_VERSION = '1';
