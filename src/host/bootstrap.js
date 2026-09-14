6bb0fb929ad9af79cc49f5a38846793f73c22cbf
Brighton Mzacana <markertwithbrighton@gmail.com>
2026-09-14 16:29:01 +0200

    refactor: compose financial service through provider port

--- src/host/bootstrap.js
+++ src/host/bootstrap.js
@@ -3,12 +3,14 @@ import * as onboardingAdapter from '../adapters/network/onboard.js';
 import * as databaseAdapter from '../adapters/persistence/database.js';
 import * as firebaseAdapter from '../adapters/persistence/firebase.js';
 import BillingAdapter from '../adapters/payments/universal-billing.js';
+import FinancialService from '../services/financial.js';
 import MikroTikAdapter from '../adapters/network/mikrotik-adapter.js';
 import { registerNetworkProvider } from '../core/mikrotik.js';
 import { registerOnboardingProvider } from '../core/onboard.js';
 import { registerDatabaseProvider } from '../core/database.js';
 import { registerPersistenceProvider } from '../core/firebase.js';
 import { registerBillingProvider } from '../core/universal-billing.js';
+import { registerFinancialProvider } from '../core/financial.js';
 import nodeRegistry from '../core/node-registry.js';
 import pluginRegistry from '../plugins/registry.js';
 import PluginManager from '../plugins/manager.js';
@@ -20,9 +22,10 @@ export function bootstrapAgentOS() {
   registerDatabaseProvider(databaseAdapter);
   registerPersistenceProvider(firebaseAdapter.default || firebaseAdapter);
   registerBillingProvider(BillingAdapter);
+  registerFinancialProvider(FinancialService);
   nodeRegistry.setManagerFactory(networkAdapter.createManager);
   pluginRegistry.register('mikrotik', MikroTikAdapter);
-  return { networkAdapter, onboardingAdapter, databaseAdapter, firebaseAdapter, BillingAdapter, nodeRegistry, pluginRegistry };
+  return { networkAdapter, onboardingAdapter, databaseAdapter, firebaseAdapter, BillingAdapter, FinancialService, nodeRegistry, pluginRegistry };
 }
 
 export function createExtensionRuntime(agent, options = {}) {
