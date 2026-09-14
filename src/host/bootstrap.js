74d424f03feb12651231c8c8b698927467e28ac0
Brighton Mzacana <markertwithbrighton@gmail.com>
2026-09-14 16:28:33 +0200

    refactor: compose billing adapter through provider port

--- src/host/bootstrap.js
+++ src/host/bootstrap.js
@@ -2,25 +2,27 @@ import * as networkAdapter from '../adapters/network/mikrotik.js';
 import * as onboardingAdapter from '../adapters/network/onboard.js';
 import * as databaseAdapter from '../adapters/persistence/database.js';
 import * as firebaseAdapter from '../adapters/persistence/firebase.js';
+import BillingAdapter from '../adapters/payments/universal-billing.js';
 import MikroTikAdapter from '../adapters/network/mikrotik-adapter.js';
 import { registerNetworkProvider } from '../core/mikrotik.js';
 import { registerOnboardingProvider } from '../core/onboard.js';
 import { registerDatabaseProvider } from '../core/database.js';
 import { registerPersistenceProvider } from '../core/firebase.js';
+import { registerBillingProvider } from '../core/universal-billing.js';
 import nodeRegistry from '../core/node-registry.js';
 import pluginRegistry from '../plugins/registry.js';
 import PluginManager from '../plugins/manager.js';
 import ServiceRegistry from '../services/registry.js';
 
-/** Composition root: concrete services/adapters enter the domain-neutral kernel here. */
 export function bootstrapAgentOS() {
   registerNetworkProvider(networkAdapter);
   registerOnboardingProvider(onboardingAdapter.default || onboardingAdapter);
   registerDatabaseProvider(databaseAdapter);
   registerPersistenceProvider(firebaseAdapter.default || firebaseAdapter);
+  registerBillingProvider(BillingAdapter);
   nodeRegistry.setManagerFactory(networkAdapter.createManager);
   pluginRegistry.register('mikrotik', MikroTikAdapter);
-  return { networkAdapter, onboardingAdapter, databaseAdapter, firebaseAdapter, nodeRegistry, pluginRegistry };
+  return { networkAdapter, onboardingAdapter, databaseAdapter, firebaseAdapter, BillingAdapter, nodeRegistry, pluginRegistry };
 }
 
 export function createExtensionRuntime(agent, options = {}) {
