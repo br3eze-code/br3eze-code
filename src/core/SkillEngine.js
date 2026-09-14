1589824cfc4afa1603d6b525bd05227c02c0be7b
Brighton Mzacana <markertwithbrighton@gmail.com>
2026-09-14 20:22:38 +0200

    Remove deprecated SkillEngine compatibility layer

--- src/core/SkillEngine.js
+++
@@ -1,5 +0,0 @@
-/** @deprecated Use the canonical Core SkillRegistry. */
-import SkillRegistry from './SkillRegistry.js';
-export { SkillRegistry };
-export { SkillRegistry as SkillEngine };
-export default SkillRegistry;
